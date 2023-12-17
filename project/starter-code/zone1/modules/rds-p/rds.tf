d697ec0d0ab5ed4870677102bc6a7af0e02253e3
Jees Antony <53956834+jeesap@users.noreply.github.com>
2023-12-17 21:22:50 +0530

    Update rds.tf

--- project/starter-code/zone1/modules/rds-p/rds.tf
+++ project/starter-code/zone1/modules/rds-p/rds.tf
@@ -45,6 +45,7 @@ resource "aws_rds_cluster" "udacity_cluster" {
   engine                   = "aurora-mysql"
   engine_mode              = "provisioned"
   engine_version           = "5.7.mysql_aurora.2.07.9" 
+  backup_retention_period  = 5
   skip_final_snapshot      = true
   storage_encrypted        = false
   depends_on = [aws_rds_cluster_parameter_group.cluster_pg]
