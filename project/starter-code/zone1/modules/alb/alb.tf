7f40f1eb6e96f848d2dcda554cff912bbdcc2a05
Jees Antony <53956834+jeesap@users.noreply.github.com>
2023-12-17 08:47:40 +0530

    Update alb.tf

--- project/starter-code/zone1/modules/alb/alb.tf
+++ project/starter-code/zone1/modules/alb/alb.tf
@@ -1,19 +1,19 @@
-resource "aws_lb_target_group" "udacityTGeast" {
-  name     = "udacity-lb-east2"
+resource "aws_lb_target_group" "udacity" {
+  name     = "udacity-lb-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
 
 resource "aws_lb_target_group_attachment" "udacity" {
-  count            = 3     
-  target_group_arn = aws_lb_target_group.udacityTGeast.arn
+  count            = 2
+  target_group_arn = aws_lb_target_group.udacity.arn
   target_id        = var.ec2.*.id[count.index]
   port             = 80
 }
 
 resource "aws_lb" "udacity" {
-  name               = "udacity-lb-alb-east-tf"
+  name               = "udacity-lb-tf"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [var.ec2_sg]
@@ -30,6 +30,6 @@ resource "aws_lb_listener" "udacity" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.udacityTGeast.arn
+    target_group_arn = aws_lb_target_group.udacity.arn
   }
 }
